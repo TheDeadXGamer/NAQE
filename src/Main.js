0e76baafa4093e887bb22c54df7c18cf557f4770
Stellio-Makrygiannis <stellio@chalmers.se>
2025-04-11 21:16:52 +0200

    Main file

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import App from './App';
+
+function Main() {
+  console.log('Main app starting...');
+
+  return (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+export default Main;
