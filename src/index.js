aa61760a9b2e774c8c71bed9a45176036ceb865d
Aurit1 <78141676+Aurit1@users.noreply.github.com>
2025-04-21 00:37:02 +0200

    revert main back to index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Main from './Main';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Main />);
+root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+);
 
 reportWebVitals();
